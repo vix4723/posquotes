8162ec4d781a72868b0a8bd847d36809dfbc487a
Vix <63135033+vix4723@users.noreply.github.com>
2024-06-17 19:09:53 -0500

    setting up workspace

---
+++ posquotes/liveblocks.config.ts
@@ -0,0 +1,19 @@
+declare global {
+  interface Liveblocks {
+    // Custom user info set when authenticating with a secret key
+    UserMeta: {
+      id: string; // Accessible through `user.id`
+      info: {
+        name: string;
+        avatar: string;
+      }; // Accessible through `user.info`
+    };
+    // Custom metadata set on threads, for useThreads, useCreateThread, etc.
+    ThreadMetadata: {
+      x: number;
+      y: number;
+    };
+  }
+}
+
+export {};
